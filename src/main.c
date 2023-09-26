84b6c5c14c773ed6c4e8ef7f3721c231734a0e4e
TheLittleFish <rafasusano13@gmail.com>
2023-09-26 02:13:28 +0100

    First branch commit
    Start of the interrup investigation
    
    Description:
    - SPI code unchanged
    - Button 1 (sw0) interrupt added and functional

--- src/main.c
+++ src/main.c
@@ -14,7 +14,8 @@
 #include <zephyr/sys/printk.h>
 #include <zephyr/logging/log.h>
 // For GPIO
-//#include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/sys/util.h>
 
 // Getting the device pointers
 #include <zephyr/device.h>
@@ -63,35 +64,71 @@ char checkDivice(const struct device *dev) {
 	return 1;
 }
 
+/** 
+ * @fn void button_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
+ * @brief Callback function for the interrupt resolted from pressing 
+ * button 1 (sw0) in the nRF52 PDK board.
+ * 
+ * @param dev a device type pointer.
+ * @param cb callback indicator ???
+ * @param pins pin in which the interrupt was originated 
+ */
+void button_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
+{
+	printk("Button pressed at %" PRIu32 "\n", k_cycle_get_32());
+}
+
 int main(void) {
 	LOG_INF("Hello World! %s\n", CONFIG_BOARD);
-	
-	const struct device *dev_bme = DEVICE_DT_GET(DT_NODELABEL(spi_bme280));
-
-	if(!checkDivice(dev_bme)){
-		dev_bme = NULL;
-		return 0;
-	}
 
 	struct spi_dt_spec spec_bme = SPI_DT_SPEC_GET(DT_NODELABEL(spi_bme280), 
 			(SPI_TRANSFER_MSB | SPI_MODE_CPOL | SPI_MODE_CPHA | 
 			SPI_WORD_SET(8) | SPI_LINES_SINGLE), 0);
 	
+	if(!checkDivice(spec_bme.bus)){
+		return 0;
+	}
+
+	static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET_OR(DT_ALIAS(sw0), gpios, {0});
+	static struct gpio_callback button_cb_data;
+
+	if(!checkDivice(button.port)){
+		return 0;
+	}
+
+	int ret;
+
+	ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
+	if (ret != 0) {
+		printk("Error %d: failed to configure %s pin %d\n",
+		       ret, button.port->name, button.pin);
+		return 0;
+	}
+
+	ret = gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_ACTIVE);
+	if (ret != 0) {
+		printk("Error %d: failed to configure interrupt on %s pin %d\n",
+			ret, button.port->name, button.pin);
+		return 0;
+	}
+
+	gpio_init_callback(&button_cb_data, button_pressed, BIT(button.pin));
+	gpio_add_callback(button.port, &button_cb_data);
+
+	uint8_t addr;
+	struct spi_buf tx_buf = {.buf = &addr, .len = 1};
+	struct spi_buf_set tx = {.buffers = &tx_buf, .count = 1};
+
+	uint8_t buf[2];
+	struct spi_buf rx_buf[2] = {{.buf=&buf[0],.len=1},{.buf=&buf[1],.len=1}};
+	//struct spi_buf rx_buf = {.buf=&buf[0],.len=1};
+	struct spi_buf_set rx = {.buffers = &rx_buf, .count = ARRAY_SIZE(rx_buf)};
+
+	//const struct device *spi = device_get_binding("SPI_0");;
+
+	addr = 0xD0 | 0x80; //| 0x80;
 
 	while(1){
-		uint8_t addr;
-		struct spi_buf tx_buf = {.buf = &addr, .len = 1};
-		struct spi_buf_set tx = {.buffers = &tx_buf, .count = 1};
-
-		uint8_t buf[2];
-		struct spi_buf rx_buf[2] = {{.buf=&buf[0],.len=1},{.buf=&buf[1],.len=1}};
-		//struct spi_buf rx_buf = {.buf=&buf[0],.len=1};
-		struct spi_buf_set rx = {.buffers = &rx_buf, .count = ARRAY_SIZE(rx_buf)};
-
-		//const struct device *spi = device_get_binding("SPI_0");;
-
-		addr = 0xD0 | 0x80; //| 0x80;
-
 		int ret = spi_transceive_dt(&spec_bme, &tx, &rx);
 		if (ret) {
 			printk("\n\nspi_transceive_dt ret code: %d", ret);
@@ -101,10 +138,13 @@ int main(void) {
 		 * Shows buf[0] = 0xff and buf[0] = 0x60 becouse of full-duplex
 		 * meaning that the first buf is read at the same time the tx is being written
 		 */ 
-		printk("\n\nbuff rx(0): %x", buf[0]);
-		printk("\nbuff rx(1): %x", buf[1]);
+		//printk("\n\nbuff rx(0): %x", buf[0]);
+		//printk("\nbuff rx(1): %x", buf[1]);
 
-		k_sleep(K_MSEC(5000));
+		if (buf[1] == 0x60)
+			LOG_DBG("ON");
+
+		k_sleep(K_MSEC(500));
 	}
 
 	return 0;
